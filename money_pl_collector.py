d13f4fca6df38ffa1c7236be44ee72e37171c263
jakubz <a5892731@o2.pl>
2021-06-29 14:05:18 +0200

    ...

--- money_pl_collector.py
+++ money_pl_collector.py
@@ -15,23 +15,22 @@ import requests
 from bs4 import BeautifulSoup
 from os import listdir, getcwd, chdir, walk
 from openpyxl import Workbook
+from time import gmtime, strftime
 
-
-
-
-class WSE_data:
-
+class Data:
     url = 'https://www.money.pl/gielda/spolki-gpw/'
     header_oryginal = ["Walor", "Kurs (PLN)", "Zmiana (%)", "Otwarcie", "Min", "Max",
-                       "Obrót (szt.)", "Obrót (PLN)", "Czas_aktualizacji"]
+                       "Obrót (szt.)", "Obrót (PLN)", "Czas_aktualizacji", "Data"]
     header_eng = ["Value", "Rate (PLN)", "Change (%)", "Opening", "Min", "Max",
-                  "Turnover (pcs.)", "Turnover (PLN)", "Update_time"]
+                  "Turnover (pcs.)", "Turnover (PLN)", "Update_time", "Date"]
+
+class WSE_Data_Collector(Data):
 
     def __init__(self):
         r = requests.get(self.url)
         soup = BeautifulSoup(r.text, "html.parser") # "html.parser" is for get rid of warnings > https://stackoverflow.com/questions/33511544/how-to-get-rid-of-beautifulsoup-user-warning
         headline_data = soup.find_all("div") # <div>This is heading</div>
-        self.data_table = self.translate_data(headline_data)
+        self.table = self.translate_data(headline_data)
 
     def translate_data(self, headline_data):
         number_of_headline = 1
@@ -62,11 +61,48 @@ class WSE_data:
             column_number += 1
 
         return table
+
+class XLS_Creator(Data):
+
+    def __init__(self, row):
+        self.row = {}
+        self.current_date =  strftime("%Y-%m-%d", gmtime())
+
+        for i in range(9):
+            self.row[self.header_oryginal[i]] = row[i]
+
+        self.time_corrector()
+
+    def create_xls_file(self, columns, title = "Arkusz 1"):
+        wb = Workbook()
+        ws = wb.active
+
+        ws.title = title
+
+        for column in range(len(columns)):
+            ws.column_dimensions[columns[column]].width = 80
+        ws.append(('Lokalizacja folderu docelowego:', getcwd()))
+        ws.append(('-----------------', '-----------------'))
+        ws.append(('Plik:', ('Folder: ' + '> > >')))
+        #ws.cell(row=3, column=max(bufor_column)+1, value='Pełny adres:')
+
+
+
+    def time_corrector(self):
+        if ":" in self.row["{}".format(self.header_oryginal[8])]:  # Czas_aktualizacji / Update_time
+            self.row["{}".format(self.header_oryginal[9])] = self.current_date # Data / Date
+        else:
+            self.row["{}".format(self.header_oryginal[9])] = self.row["{}".format(self.header_oryginal[8])]
+            self.row["{}".format(self.header_oryginal[8])] = "—"
+
+
+
 #-----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
 
-    GPW = WSE_data()
-    print(GPW.header_oryginal)
-    for row in GPW.data_table:
-        print(row)
+    GPW = WSE_Data_Collector()
+    for row in GPW.table:
+        data_storage = XLS_Creator(row)
+        print(data_storage.row)
+
